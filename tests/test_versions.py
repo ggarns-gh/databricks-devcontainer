2e698515c81893a34ad7951f82f9a20e8272b150
ggarns-gh <70338276+ggarns-gh@users.noreply.github.com>
2024-04-21 20:41:48 +0100

    Format tests

--- tests/test_versions.py
+++ tests/test_versions.py
@@ -3,7 +3,11 @@ import importlib_metadata
 
 
 def test_python_version_is_310():
-    assert sys.version_info.major == 3 and sys.version_info.minor == 10 and sys.version_info.micro >= 12
+    assert (
+        sys.version_info.major == 3
+        and sys.version_info.minor == 10
+        and sys.version_info.micro >= 12
+    )
 
 
 def test_delta_version_is_310():
